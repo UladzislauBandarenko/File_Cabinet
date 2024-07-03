e2239d20c6962df6b372493469414ee6c9325452
UladzislauBandarenko <bandarenka.uladzislau@student.ehu.lt>
2024-07-03 20:29:35 +0300

    Extend FileCabinetRecord class with new properties - Age, Salary, Gender. Update Create and List methods accordingly.

--- FileCabinetApp/FileCabinetRecord.cs
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -1,10 +1,18 @@
+namespace FileCabinetApp;
+
 public class FileCabinetRecord
 {
     public int Id { get; set; }
 
-    public string FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
 
     public DateTime DateOfBirth { get; set; }
+
+    public short Age { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public char Gender { get; set; }
 }
--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -1,22 +1,34 @@
+namespace FileCabinetApp;
+
 public class FileCabinetService
 {
     private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
 
-    public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+    public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char gender)
     {
-        // TODO: добавьте реализацию метода
-        return 0;
+        var record = new FileCabinetRecord
+        {
+            Id = this.list.Count + 1,
+            FirstName = firstName,
+            LastName = lastName,
+            DateOfBirth = dateOfBirth,
+            Age = age,
+            Salary = salary,
+            Gender = gender,
+        };
+
+        this.list.Add(record);
+
+        return record.Id;
     }
 
     public FileCabinetRecord[] GetRecords()
     {
-        // TODO: добавьте реализацию метода
         return Array.Empty<FileCabinetRecord>();
     }
 
     public int GetStat()
     {
-        // TODO: добавьте реализацию метода
-        return 0;
+        return this.list.Count;
     }
 }

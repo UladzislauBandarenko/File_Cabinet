b7bdf3a4da04bd11a7e3da1b8d8be6d918490b5e
UladzislauBandarenko <bandarenka.uladzislau@student.ehu.lt>
2024-07-04 01:25:32 +0300

    Move FileCabinetService to FileCabinetMemoryService. Add FileCabinetFileSystemService.

---
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -0,0 +1,47 @@
+using System.Collections.ObjectModel;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetFilesystemService : IFileCabinetService
+    {
+        public int CreateRecord(PersonalInfo personalInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetStat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditRecord(int id, PersonalInfo personalInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByDateOfBirth(string dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileCabinetServiceSnapshot MakeSnapshot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

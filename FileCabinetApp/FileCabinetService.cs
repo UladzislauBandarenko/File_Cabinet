fba16e86ab3de86d79025793029b422ec6be34d0
UladzislauBandarenko <bandarenka.uladzislau@student.ehu.lt>
2024-07-03 20:34:05 +0300

    FileCabinetService.CreateRecord method added input validation

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -6,6 +6,36 @@ public class FileCabinetService
 
     public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char gender)
     {
+        if (string.IsNullOrWhiteSpace(firstName) || firstName.Length < 2 || firstName.Length > 60)
+        {
+            throw new ArgumentException("First name must be between 2 and 60 characters and not empty.", nameof(firstName));
+        }
+
+        if (string.IsNullOrWhiteSpace(lastName) || lastName.Length < 2 || lastName.Length > 60)
+        {
+            throw new ArgumentException("Last name must be between 2 and 60 characters and not empty.", nameof(lastName));
+        }
+
+        if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+        {
+            throw new ArgumentException("Date of birth must be between 01-Jan-1950 and the current date.", nameof(dateOfBirth));
+        }
+
+        if (age < 0 || age > 120)
+        {
+            throw new ArgumentException("Age must be between 0 and 120.", nameof(age));
+        }
+
+        if (salary < 0 || salary > 1000000)
+        {
+            throw new ArgumentException("Salary must be between 0 and 1,000,000.", nameof(salary));
+        }
+
+        if (gender != 'M' && gender != 'F')
+        {
+            throw new ArgumentException("Gender must be either 'M' or 'F'.", nameof(gender));
+        }
+
         var record = new FileCabinetRecord
         {
             Id = this.list.Count + 1,

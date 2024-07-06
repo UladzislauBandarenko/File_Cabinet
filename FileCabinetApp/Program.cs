b971dcc4b0478ab5bd1193080c49b2207de4b71d
UladzislauBandarenko <bandarenka.uladzislau@student.ehu.lt>
2024-07-06 11:57:48 +0300

    extracted some logic from the main method into seperate private methods. Created new HelpMessages model

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.ObjectModel;
-using System.Globalization;
-using FileCabinetApp.CommandHandlers;
+﻿using FileCabinetApp.CommandHandlers;
 using FileCabinetApp.Models;
 using FileCabinetApp.Validators;
-using Microsoft.Extensions.Configuration;
 
 namespace FileCabinetApp;
 
@@ -12,20 +9,7 @@ namespace FileCabinetApp;
 /// </summary>
 public static class Program
 {
-    private static readonly IReadOnlyCollection<HelpMessage> HelpMessages = new[]
-    {
-        new HelpMessage("help", "prints the help screen", "The 'help' command prints the help screen."),
-        new HelpMessage("exit", "exits the application", "The 'exit' command exits the application."),
-        new HelpMessage("stat", "prints the number of records", "The 'stat' command prints the number of records."),
-        new HelpMessage("create", "creates a new record", "The 'create' command creates a new record."),
-        new HelpMessage("export", "exports records to a file", "The 'export' command exports all records to a file. Usage: export [csv|xml] <filename>"),
-        new HelpMessage("import", "imports records from a file", "The 'import' command imports records from a file. Usage: import [csv|xml] <filename>"),
-        new HelpMessage("purge", "purges all records", "The 'purge' command purges all records."),
-        new HelpMessage("insert", "inserts a new record", "The 'insert' command inserts a new record. Usage: insert (id, firstname, lastname, dateofbirth, height, weight, gender) values (<id>, <firstname>, <lastname>, <dateofbirth>, <height>, <weight>, <gender>)"),
-        new HelpMessage("delete", "deletes records", "The 'delete' command deletes records. Usage: delete where <field>=<value>"),
-        new HelpMessage("update", "updates records", "The 'update' command updates records. Usage: update set <field1>=<value1>, <field2>=<value2>, ... where <field3>=<value3>, <field4>=<value4>, ..."),
-        new HelpMessage("select", "selects records", "The 'select' command selects records based on specified fields and conditions. Usage: select <field1>, <field2>, ... [where <condition1> and/or <condition2> ...]"),
-    };
+    private static readonly IReadOnlyCollection<HelpMessage> HelpMessages = Models.HelpMessages.Messages;
 
     private static IFileCabinetService? fileCabinetService;
 
@@ -121,58 +105,11 @@ public static class Program
             fileCabinetService = new ServiceLogger(fileCabinetService, logFilePath);
         }
 
-        Console.WriteLine($"File Cabinet Application, developed by {AppConstants.DeveloperName}");
-        Console.WriteLine($"Using {storage} storage.");
-        Console.WriteLine($"Using {validationRules} validation rules.");
-        if (useStopwatch)
-        {
-            Console.WriteLine("Execution time measurement is enabled.");
-        }
-
-        if (useLogger)
-        {
-            Console.WriteLine("Service logging is enabled.");
-        }
-
-        Console.WriteLine(AppConstants.HintMessage);
-        Console.WriteLine();
+        PrintStartupMessages(storage, validationRules, useStopwatch, useLogger);
 
         var commandHandler = CreateCommandHandlers(fileCabinetService);
 
-        do
-        {
-            Console.Write("> ");
-            var command = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(command))
-            {
-                Console.WriteLine(AppConstants.HintMessage);
-                continue;
-            }
-
-            var commandName = command.Split(' ')[0].ToLowerInvariant();
-            var validCommands = HelpMessages.Select(m => m.Command.ToLowerInvariant()).ToList();
-
-            if (validCommands.Contains(commandName))
-            {
-                commandHandler.Handle(command);
-            }
-            else
-            {
-                var similarCommands = FindSimilarCommands(commandName, validCommands);
-                Console.WriteLine($"'{commandName}' is not a valid command. See 'help' for available commands.");
-
-                if (similarCommands.Any())
-                {
-                    Console.WriteLine("The most similar command" + (similarCommands.Count > 1 ? "s are" : " is"));
-                    foreach (var similarCommand in similarCommands)
-                    {
-                        Console.WriteLine($"\t{similarCommand}");
-                    }
-                }
-            }
-        }
-        while (isRunning);
+        RunCommandLoop(commandHandler);
     }
 
     private static int LevenshteinDistance(string s, string t)
@@ -254,5 +191,62 @@ public static class Program
         updateCommandHandler.SetNext(selectCommandHandler);
 
         return helpHandler;
+    }
+
+    private static void PrintStartupMessages(string storage, string validationRules, bool useStopwatch, bool useLogger)
+    {
+        Console.WriteLine($"File Cabinet Application, developed by {AppConstants.DeveloperName}");
+        Console.WriteLine($"Using {storage} storage.");
+        Console.WriteLine($"Using {validationRules} validation rules.");
+        if (useStopwatch)
+        {
+            Console.WriteLine("Execution time measurement is enabled.");
+        }
+
+        if (useLogger)
+        {
+            Console.WriteLine("Service logging is enabled.");
+        }
+
+        Console.WriteLine(AppConstants.HintMessage);
+        Console.WriteLine();
+    }
+
+    private static void RunCommandLoop(ICommandHandler commandHandler)
+    {
+        do
+        {
+            Console.Write("> ");
+            var command = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(command))
+            {
+                Console.WriteLine(AppConstants.HintMessage);
+                continue;
+            }
+
+            var commandName = command.Split(' ')[0].ToLowerInvariant();
+            var validCommands = HelpMessages.Select(m => m.Command.ToLowerInvariant()).ToList();
+
+            if (validCommands.Contains(commandName))
+            {
+                commandHandler.Handle(command);
+            }
+            else
+            {
+                var similarCommands = FindSimilarCommands(commandName, validCommands);
+                Console.WriteLine($"'{commandName}' is not a valid command. See 'help' for available commands.");
+
+                if (similarCommands.Any())
+                {
+                    Console.WriteLine("The most similar command" + (similarCommands.Count > 1 ? "s are" : " is"));
+                    foreach (var similarCommand in similarCommands)
+                    {
+                        Console.WriteLine($"\t{similarCommand}");
+                    }
+                }
+            }
+        }
+        while (isRunning);
     }
 }
